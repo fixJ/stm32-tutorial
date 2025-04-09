c3846174a02cbb666404ec9a436c5224164a1f72
jx <jxbebetter@gmail.com>
2025-04-09 13:46:14 +0800

    feat: minibink

--- miniblink/miniblink.c
+++ miniblink/miniblink.c
@@ -4,12 +4,12 @@
 static void
 gpio_setup(void) {
 
-    /* Enable GPIOC clock. */
-    rcc_periph_clock_enable(RCC_GPIOC);
+    /* Enable GPIOE clock. */
+    rcc_periph_clock_enable(RCC_GPIOE);
 
     /* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
-    gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+    gpio_set_mode(GPIOE,GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
 }
 
 int
@@ -19,11 +19,11 @@ main(void) {
     gpio_setup();
 
     for (;;) {
-        gpio_clear(GPIOC,GPIO13);	/* LED on */
+        gpio_clear(GPIOE,GPIO5);	/* LED on */
         for (i = 0; i < 1500000; i++)	/* Wait a bit. */
             __asm__("nop");
 
-        gpio_set(GPIOC,GPIO13);		/* LED off */
+        gpio_set(GPIOE,GPIO5);		/* LED off */
         for (i = 0; i < 500000; i++)	/* Wait a bit. */
             __asm__("nop");
     }
