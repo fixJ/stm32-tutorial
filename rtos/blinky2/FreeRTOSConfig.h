b9df7147202214f334cb5c789efe31b44190710c
jx <jxbebetter@gmail.com>
2025-04-10 14:21:45 +0800

    feat: blink with os

--- rtos/blinky2/FreeRTOSConfig.h
+++ rtos/blinky2/FreeRTOSConfig.h
@@ -1,3 +1,10 @@
 #define    configMAX_PRIORITIES    5
 #define    configMINIMAL_STACK_SIZE    (unsigned short) 128
 #define    configCHECK_FOR_STACK_OVERFLOW     1
+#define    configUSE_16_BIT_TICKS
+#define    configTICK_TYPE_WIDTH_IN_BITS
+#define    configMAX_SYSCALL_INTERRUPT_PRIORITY
+#define    configUSE_PREEMPTION
+#define    configUSE_IDLE_HOOK
+#define    configUSE_TICK_HOOK
+#define    configTICK_RATE_HZ
