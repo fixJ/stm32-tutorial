1e25c340b8f74a815119806aa1535609eaaa1d44
jx <jxbebetter@gmail.com>
2025-04-10 13:56:41 +0800

    feat: blink with os

---
+++ rtos/blinky2/FreeRTOSConfig.h
@@ -0,0 +1,4 @@
+#include
+#define    configMAX_PRIORITIES    5
+#define    configMINIMAL_STACK_SIZE    (unsigned short) 128
+#define    configCHECK_FOR_STACK_OVERFLOW     1
---
+++ rtos/blinky2/main.c
@@ -0,0 +1,27 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask __attribute((unused)), char * pcTaskName __attribute((unused))) {
+    for(;;);
+}
+
+static void task1(void *args __attribute((unused))) {
+    for(;;){
+      gpio_toggle(GPIOE, GPIO5);
+      vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+int main(void) {
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_set_mode(GPIOE,GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
+    xTaskCreate(task1, "led", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-1, NULL);
+    vTaskStartScheduler();
+    for(;;);
+    return 0;
+}
