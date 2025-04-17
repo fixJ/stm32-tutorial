0e6c3ca54552eb76875d52c3c5fc2c7fee412443
jx <jxbebetter@gmail.com>
2025-04-17 22:34:13 +0800

    feat: usb

--- rtos/usbcdc/main.c
+++ rtos/usbcdc/main.c
@@ -20,11 +20,26 @@ static void receive_task(void *args __attribute__((unused))) {
     }
 }
 
+static void led_task(void *args __attribute__((unused))) {
+  for(;;) {
+      gpio_toggle(GPIOE);
+      vTaskDelay(pdMS_TO_TICKS(2000));
+  }
+}
+
+static void led_setup() {
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_set_mode(GPIOE,GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
+}
+
 
 int main(void) {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+    led_setup();
     usb_start();
     xTaskCreate(receive_task, "receive", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(led_task, "led", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-2, NULL);
     vTaskStartScheduler();
     for(;;);
     return 0;
