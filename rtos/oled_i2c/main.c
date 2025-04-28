dbd9243bdc052142ce17666e56bec3507886757e
jx <jxbebetter@gmail.com>
2025-04-28 14:44:55 +0800

    feat: oled with i2c

--- rtos/oled_i2c/main.c
+++ rtos/oled_i2c/main.c
@@ -10,7 +10,7 @@ static I2C_Control i2c_device;
 
 int main(void) {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
-    i2c_configure(i2c_device, I2C1, 1000);
+    i2c_configure(&i2c_device, I2C1, 1000);
     for(;;);
     return 0;
 }
