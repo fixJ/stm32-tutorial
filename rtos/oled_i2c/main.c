66214b69d9c6ccf26c621d94874d6e8e3ae4acaf
jx <jxbebetter@gmail.com>
2025-04-28 14:44:15 +0800

    feat: oled with i2c

--- rtos/oled_i2c/main.c
+++ rtos/oled_i2c/main.c
@@ -3,31 +3,14 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+#include "i2c.h"
 
-void vApplicationStackOverflowHook( TaskHandle_t xTask __attribute((unused)), char * pcTaskName __attribute((unused))) {
-    for(;;);
-}
+static I2C_Control i2c_device;
 
-static void task1(void *args __attribute((unused))) {
-    gpio_set(GPIOE, GPIO5);
-    gpio_clear(GPIOB, GPIO5);
-    for(;;){
-      gpio_toggle(GPIOB, GPIO5);
-      gpio_toggle(GPIOE, GPIO5);
-      vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
 
 int main(void) {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
-    rcc_periph_clock_enable(RCC_GPIOB);
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_set_mode(GPIOB,GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
-    gpio_set_mode(GPIOE,GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
-    xTaskCreate(task1, "led", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES-1, NULL);
-    vTaskStartScheduler();
+    i2c_configure(i2c_device, I2C1, 1000);
     for(;;);
     return 0;
 }
