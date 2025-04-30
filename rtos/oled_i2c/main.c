ffdcbd9818d5311d21869099f4b87f0dbdf42100
jx <jxbebetter@gmail.com>
2025-04-30 16:07:07 +0800

    feat: oled with i2c

--- rtos/oled_i2c/main.c
+++ rtos/oled_i2c/main.c
@@ -1,16 +1,43 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "oled.h"
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
 #include "i2c.h"
+#include "ugui.h"
 
 static I2C_Control i2c_device;
+
+static void task(void * args __attribute__((unused))) {
+
+}
 
 
 int main(void) {
     rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_I2C1);
+    gpio_set_mode(GPIOA,GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL,GPIO5);
+    gpio_set_mode(GPIOB,GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,GPIO6|GPIO7);
+    gpio_set(GPIOB,GPIO6|GPIO7);
     i2c_configure(&i2c_device, I2C1, 1000);
+    oled_init(&i2c_device);
+
+    oled_command2(0x20,0x02);// Page mode
+    oled_command(0x40);
+    oled_command2(0xD3,0x00);
+    for ( uint8_t px=0; px<8; ++px ) {
+        oled_command(0xB0|px);
+        oled_command(0x00); // Lo col
+        oled_command(0x10); // Hi col
+        for ( unsigned bx=0; bx<128; ++bx )
+            oled_write_data(BMP1++);
+    }
+
     for(;;);
     return 0;
 }
---
+++ rtos/oled_i2c/oled.c
@@ -0,0 +1,53 @@
+#include "oled.h"
+#include "task.h"
+#include <libopencm3/stm32/gpio.h>
+
+/*
+* pin description
+* GND -> GND
+* VCC -> VCC
+* CLK -> PB6
+* MOS -> PB7
+* RES -> PA5
+* DC -> PA6 GND
+* CS -> PA7 GND
+ *
+ * */
+
+void oled_write_command(I2C_Control *dev, uint8_t command) {
+  i2c_write(dev, OLED_WRITE_CMD);
+  i2c_write(dev, command);
+  i2c_stop(dev);
+}
+
+void oled_write_command2(I2C_Control *dev, uint8_t cmd1, uint8_t cmd2) {
+  i2c_write(dev, OLED_WRITE_CMD);
+  i2c_write(dev, cmd1);
+  i2c_write(dev, cmd2);
+  i2c_stop(dev);
+}
+
+void oled_write_data(I2C_Control *dev, uint8_t data) {
+  i2c_write(dev, OLED_WRITE_DATA);
+  i2c_write(dev, data);
+  i2c_stop(dev);
+}
+
+void oled_reset(void) {
+  gpio_clear(GPIOA, GPIO5);
+  vTaskDelay(5);
+  gpio_set(GPIOA, GPIO5);
+}
+
+
+void oled_init(I2C_Control *dev) {
+  static uint8_t cmds[] = {
+    0xAE, 0x00, 0x10, 0x40, 0x81, 0xCF, 0xA1, 0xA6,
+    0xA8, 0x3F, 0xD3, 0x00, 0xD5, 0x80, 0xD9, 0xF1,
+    0xDA, 0x12, 0xDB, 0x40, 0x8D, 0x14, 0xAF, 0xFF
+  };
+  oled_reset();
+  for (uint8_t i = 0; cmds[i] != 0xff; i++) {
+    oled_write_command(dev, cmds[i]);
+  }
+}
